200b25b0bdc309db8cb92d0c104116b71bde258f
Daisuke <daisukeokahassou@gmail.com>
2017-05-31 22:16:36 +0900

    Initial Commit

---
+++ DOFlatButtonProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DOFlatButtonProj
+//
+//  Created by 岡大輔 on 2017/05/31.
+//  Copyright © 2017年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
